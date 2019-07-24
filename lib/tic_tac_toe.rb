627bffaa8315692388cdaf00a630c6909abd4cd8
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-24 14:33:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,13 +52,13 @@ def play(board)
 end
 
 def turn_count(board)
-  count = 0
+  @count = 0
   board.each do |space|
     if space == "X" || space == "O"
-      count += 1
+      @count += 1
     end
   end
-  count
+  @count
 end
 
 def current_player(board)
